c8e346084aee5b7499dc0a2bf463abe60c0d7743
katarzynakaz <l453r3@gmail.com>
2026-02-20 21:47:05 +0000

    letters task

--- Sprint-2/improve_with_precomputing/count_letters/count_letters.py
+++ Sprint-2/improve_with_precomputing/count_letters/count_letters.py
@@ -1,12 +1,35 @@
+# def count_letters(s: str) -> int:
+#     """
+#     count_letters returns the number of letters which only occur in upper case in the passed string.
+#     """
+#     only_upper = set()
+#     for letter in s:
+#         if is_upper_case(letter):
+#             if letter.lower() not in s:
+#                 only_upper.add(letter)
+#     return len(only_upper)
+
+
+# def is_upper_case(letter: str) -> bool:
+#     return letter == letter.upper()
+
+
 def count_letters(s: str) -> int:
     """
     count_letters returns the number of letters which only occur in upper case in the passed string.
     """
-    only_upper = set()
+    seen_in_string = {}
+
+    for letter in s:
+        #emptyval for key because found by key
+        seen_in_string[letter] = True
+
+    only_upper = {}
     for letter in s:
         if is_upper_case(letter):
-            if letter.lower() not in s:
-                only_upper.add(letter)
+            if letter.lower() not in seen_in_string:
+                    only_upper[letter] = True
+
     return len(only_upper)
 
 
