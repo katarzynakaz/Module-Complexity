9c663067b17d4238427a7ac185119d5bc80ca2d7
katarzynakaz <l453r3@gmail.com>
2026-02-20 21:07:43 +0000

    fibonqacci

--- Sprint-2/improve_with_caches/fibonacci/fibonacci.py
+++ Sprint-2/improve_with_caches/fibonacci/fibonacci.py
@@ -1,4 +1,23 @@
+# def fibonacci(n):
+#     if n <= 1:
+#         return n
+#     return fibonacci(n - 1) + fibonacci(n - 2)
+
+#store known values
+known_values = {
+}
+
 def fibonacci(n):
+    if n in known_values:
+        return known_values[n]
+    
+
     if n <= 1:
         return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    else:
+        
+        result = fibonacci(n - 1) + fibonacci(n - 2)
+        
+        known_values[n] = result
+        
+        return result
