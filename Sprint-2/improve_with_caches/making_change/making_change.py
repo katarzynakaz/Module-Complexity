46e479f3f149177b138dfab009b377f7ee18f9bd
katarzynakaz <l453r3@gmail.com>
2026-02-20 21:18:31 +0000

    change task

--- Sprint-2/improve_with_caches/making_change/making_change.py
+++ Sprint-2/improve_with_caches/making_change/making_change.py
@@ -1,5 +1,39 @@
-from typing import List
+# from typing import List
 
+
+# def ways_to_make_change(total: int) -> int:
+#     """
+#     Given access to coins with the values 1, 2, 5, 10, 20, 50, 100, 200, returns a count of all of the ways to make the passed total value.
+
+#     For instance, there are two ways to make a value of 3: with 3x 1 coins, or with 1x 1 coin and 1x 2 coin.
+#     """
+#     return ways_to_make_change_helper(total, [200, 100, 50, 20, 10, 5, 2, 1])
+
+
+# def ways_to_make_change_helper(total: int, coins: List[int]) -> int:
+#     """
+#     Helper function for ways_to_make_change to avoid exposing the coins parameter to callers.
+#     """
+#     if total == 0 or len(coins) == 0:
+#         return 0
+
+#     ways = 0
+#     for coin_index in range(len(coins)):
+#         coin = coins[coin_index]
+#         count_of_coin = 1
+#         while coin * count_of_coin <= total:
+#             total_from_coins = coin * count_of_coin
+#             if total_from_coins == total:
+#                 ways += 1
+#             else:
+#                 intermediate = ways_to_make_change_helper(total - total_from_coins, coins=coins[coin_index+1:])
+#                 ways += intermediate
+#             count_of_coin += 1
+#     return ways
+
+
+from typing import List
+already_known={}
 
 def ways_to_make_change(total: int) -> int:
     """
@@ -7,8 +41,14 @@ def ways_to_make_change(total: int) -> int:
 
     For instance, there are two ways to make a value of 3: with 3x 1 coins, or with 1x 1 coin and 1x 2 coin.
     """
-    return ways_to_make_change_helper(total, [200, 100, 50, 20, 10, 5, 2, 1])
-
+    #cehck in in known 
+    if total in already_known:
+        return already_known[total]
+    
+    result = ways_to_make_change_helper(total, [200, 100, 50, 20, 10, 5, 2, 1])
+    already_known[total] = result
+    # //add
+    return result
 
 def ways_to_make_change_helper(total: int, coins: List[int]) -> int:
     """
